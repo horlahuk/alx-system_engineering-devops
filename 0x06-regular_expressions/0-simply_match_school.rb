3bbe78fe3d5628b47f3dac305a2f05d4d71dea14
horlahuk <olaitan.u.k@gmail.com>
2023-07-04 05:47:19 -0700

    add task 0 file

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,6 @@
+#!/usr/bin/env ruby
+# Using the project instructions, create a Ruby script that accepts
+# one argument and pass it to a regular expression matching method
+# The regular expression must match School
+
+puts ARGV[0].scan(/School/).join
