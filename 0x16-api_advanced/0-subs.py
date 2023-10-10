f24ed41929c43fb56914ec9d8ea5a2b0ddaa0c56
horlahuk <olaitan.u.k@gmail.com>
2023-10-10 04:31:37 -0700

    Task 0

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" queries the Reddit API and returns the number of subscribers"""
+
+import requests
+
+def number_of_subscribers(subreddit):
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-agent":"MyApp/1.0"}
+
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        count = data.get("data").get("subscribers")
+        return count
+    return 0
